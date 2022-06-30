201a30c08139723222d6c2adfc8aecfc7108787d
Shunzhe <git@mszmagic.com>
2022-06-30 16:50:43 +0900

    Added horizontal basic information view with a system SF Symbol image

---
+++ Sources/SwiftUILibrary/HorizontalBasicInfo_withSystemImage.swift
@@ -0,0 +1,42 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Sora on 2022/06/30.
+//
+
+import SwiftUI
+
+@available(iOS 13.0, *)
+@available(macOS 11.0, *)
+@available(watchOS 6.0, *)
+public struct HorizontalBasicInfo_withSystemImage: View {
+    
+    private var systemImageName: String
+    private var title: String
+    private var content: String
+    
+    public init(title: String, content: String, systemImageName: String) {
+        self.title = title
+        self.content = content
+        self.systemImageName = systemImageName
+    }
+    
+    public var body: some View {
+        
+        HStack {
+            
+            Label(title, systemImage: systemImageName)
+                .font(.headline)
+            
+            Spacer()
+            
+            Text(content)
+                .multilineTextAlignment(.trailing)
+            
+        }
+        
+    }
+    
+}
+
