63fb6f1ae013ff5d7cced6dd1089e13a1a558177
MszPro <code_sign@mszpro.com>
2024-03-21 16:03:26 +0900

    Added flippable view

--- Sources/SwiftUILibrary.docc/SwiftUILibrary.md
+++
@@ -1,3 +0,0 @@
-# ``SwiftUILibrary``
-
-Catalog sloths you find in nature and create new adorable virtual sloths.
---
+++ Sources/SwiftUILibrary/FlippableView.swift
@@ -0,0 +1,65 @@
+//
+//  FlippableView.swift
+//  AIVocabularyBook
+//
+//  Created by Msz on 2023/05/10.
+//
+
+import SwiftUI
+
+@available(iOS 13.0, *)
+@available(macOS 11, *)
+@available(watchOS 6.0, *)
+struct FlippableView<Front: View, Back: View>: View {
+    
+    var cardFrontView: Front
+    var cardBackView: Back
+    
+    @State private var isFlipped = false
+    private let animationDuration = 0.3
+    
+    init(@ViewBuilder front: () -> Front, @ViewBuilder back: () -> Back) {
+        self.cardFrontView = front()
+        self.cardBackView = back()
+    }
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            ZStack {
+                if isFlipped {
+                    cardBackView
+                        .rotation3DEffect(.degrees(180), axis: (x: 0.0, y: 1.0, z: 0.0))
+                } else {
+                    cardFrontView
+                }
+            }
+            .rotation3DEffect(.degrees(isFlipped ? 180 : 0), axis: (x: 0.0, y: 1.0, z: 0.0))
+            .animation(.easeInOut(duration: animationDuration), value: isFlipped)
+            .onTapGesture {
+                withAnimation {
+                    self.isFlipped.toggle()
+                }
+            }
+            Spacer()
+        }
+    }
+}
+
+
+struct FlippableView_Previews: PreviewProvider {
+    static var previews: some View {
+        FlippableView {
+            Text("Front of card")
+                .padding()
+                .foregroundColor(.white)
+                .background(RoundedRectangle(cornerRadius: 10).foregroundColor(.blue))
+        } back: {
+            Text("Back of card")
+                .padding()
+                .foregroundColor(.white)
+                .background(RoundedRectangle(cornerRadius: 10).foregroundColor(.red))
+        }
+
+    }
+}
